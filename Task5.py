98995ba89212e21e77b42008073c3df33780fb88
Kotholla Sankaraiah Manjunadh <manjunadhkotholla08@gmail.com>
2026-06-01 21:48:31 +0530

    Create Task5.py
    This task  tell us us to Intern  understands model evaluation fundamentals

---
+++ Task5.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+    classification_report
+)
+
+# Load Dataset
+df = pd.read_csv("heart.csv")
+
+# Features and Target
+X = df.drop("target", axis=1)
+y = df["target"]
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train Model
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Metrics
+print("Accuracy :", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall   :", recall_score(y_test, y_pred))
+
+# Confusion Matrix
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+# Classification Report
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
